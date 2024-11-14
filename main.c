1a5e76a062042ba8eabf539d27e218bb693e54e5
Lucas Cozzer <161613624+LucasCozzer@users.noreply.github.com>
2024-11-14 12:40:00 -0300

    Estruturando projeto

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+// define
+#define MAX_LEGTH_LIST 10; // tamanho máximo da lista
+
+// struct
+
+// estrutura da tarefa
+typedef struct {
+    char name_task[50 + 1]; // nome da tarefa
+    char description_task[50 + 1]; // descrição da tarefa
+    int status; // staus da tarefa
+    int id; // id tarefa
+} TASK;
+
+// procedimentos
+
 
 int main () {
+
 
 
     return 0;
